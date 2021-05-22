b1b5e4093c266e83617f5a35329048069b40c7a1
faisal <faisal25march99@gmail.com>
2021-05-22 10:56:36 +0530

    Issue Tracker - Composing Component

--- issue-tracker/public/src/App.jsx
+++ issue-tracker/public/src/App.jsx
@@ -1,11 +1,48 @@
-let contentNode = document.getElementById("container")
+let contentNode = document.getElementById("container");
 
 class IssueList extends React.Component {
+  render() {
+    return (
+      <div>
+       <h1>Issue Tracker</h1>
+       <IssueFilter/>
+       <hr />
+       <IssueTable />
+       <hr />
+       <IssueAdd />
+      </div>
+    );
+  }
+}
+
+class IssueFilter extends React.Component {
     render(){
         return(
-            <div>This is the placeholder for issue list...</div>
+            <div>
+                <p>This is the placeholder for Issue filter...</p>
+            </div>
         )
     }
 }
 
-ReactDOM.render(<IssueList></IssueList>, contentNode) // Render the component inside the content Node
+class IssueTable extends React.Component {
+    render(){
+        return(
+            <div>
+                <p>This is the placeholder for Issue Table...</p>
+            </div>
+        )
+    }
+}
+
+class IssueAdd extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>This is the placeholder for Issue Add entry form...</p>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<IssueList></IssueList>, contentNode); // Render the component inside the content Node
