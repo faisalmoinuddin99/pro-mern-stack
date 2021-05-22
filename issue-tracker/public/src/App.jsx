3e092e07a24ce3ce6fbb5344948ce9d212f55cd2
faisal <faisal25march99@gmail.com>
2021-05-22 11:55:44 +0530

    Issue Tracker - Passing Data: Using Property

--- issue-tracker/public/src/App.jsx
+++ issue-tracker/public/src/App.jsx
@@ -27,10 +27,41 @@ class IssueFilter extends React.Component {
 
 class IssueTable extends React.Component {
     render(){
+        const borderStyle = {border: "1px solid silver", padding: 6}
         return(
-            <div>
-                <p>This is the placeholder for Issue Table...</p>
-            </div>
+            <table style={{borderCollapse: "collapse"}}>
+                <thead>
+                    <tr>
+                        <th style={borderStyle}>Ticket_ID</th>
+                        <th style={borderStyle}>Title</th>  
+                        <th style={borderStyle}>Status</th>  
+                        <th style={borderStyle}>ClientName</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <IssueRow issue_id={1} 
+                    issue_title = {"ETL failed today"}
+                    issue_status = {"pending with client"}
+                    client_name = "RBL"
+                    />
+                    <IssueRow  issue_id = {2}
+                    issue_title = {"Please provide us a sql query"}
+                    issue_status = {"closed"}
+                    client_name = "Axis"
+                    />
+                    <IssueRow issue_id = {3}
+                    issue_title = "Error in console when clicking Add"
+                    issue_status = {"open"}
+                    client_name = "SBIG"
+                    />
+                    <IssueRow issue_id = {4}
+                    issue_title = {"Missing bottom border on panel"}
+                    issue_status = {"In-progress"}
+                    client_name = "Paytm"
+                    />
+                </tbody>
+            </table>
+          
         )
     }
 }
@@ -45,4 +76,19 @@ class IssueAdd extends React.Component {
     }
 }
 
+// Passing data from IssueTable to IssueRow Component
+class IssueRow extends React.Component {
+    render() {
+        const borderStyle = {border: "1px solid silver", padding: 4} // style object
+        const textstyle = {color: "red"}
+        return(
+            <tr>
+                <td style={borderStyle}>{this.props.issue_id}</td>
+                <td style={borderStyle}>{this.props.issue_title}</td>
+                <td style={borderStyle, textstyle}>{this.props.issue_status}</td>
+                <td style={borderStyle}>{this.props.client_name}</td>
+            </tr>
+        )
+    }
+}
 ReactDOM.render(<IssueList></IssueList>, contentNode); // Render the component inside the content Node
