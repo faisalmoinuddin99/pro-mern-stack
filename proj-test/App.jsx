0c8634a230d251a5f65a5def71833772035a8595
faisal <faisal25march99@gmail.com>
2021-05-22 07:18:34 +0530

    Chapter2- Notes with solutions

--- proj-test/App.jsx
+++ proj-test/App.jsx
@@ -1,7 +1,8 @@
 const contentNode = document.getElementById("container")
 const todos = ["go to gym","buy bread","eat","go to office"]
 
-const message = todos.map(c => `faisal ${c}!`).join(" ")
+const message = todos.map(c => `faisal ${c}!`).join("<br>")
 const component = <p>{message}</p>
+
 
 ReactDOM.render(component,contentNode)
